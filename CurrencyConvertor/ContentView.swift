6c28759b834ea920fd9fb15911b13fc4b5b6decd
Abiodun Osagie <abiodunosagie8@gmail>
2025-04-28 17:00:15 +0100

    Initial Commit

---
+++ CurrencyConvertor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyConvertor
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Text("Amount")
+                .font(.system(size: 15, weight: .semibold))
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
